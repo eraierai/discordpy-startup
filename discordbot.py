d72ae1a0fdcc7b3d6e483cff88b8ce258a8d4eb1
eraierai <84972403+eraierai@users.noreply.github.com>
2021-05-29 17:49:40 +0900

    test4

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,8 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='')
+bot = commands.Bot(command_prefix='/
+                   ')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
